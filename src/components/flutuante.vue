8e5e3c56876a76d2011cf81a5ecc4e19ab167605
caiocosta22 <caiocosta22@gmail.com>
2023-11-01 17:40:03 -0300

    ajustes gerais e adição de nova sessao

---
+++ src/components/flutuante.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template lang="pug">
+div.container
+  div.interno
+    div(style="width:45%")
+      span(style="font-size:28px; font-weight:400;text-align:left;") Tudo o que você precisa em suprimentos corporativos está aqui.&nbsp
+      span(style="font-size:28px; font-weight:bold;text-align:left;") Papelaria, limpeza, descartáveis, EPI, equipamentos e muito mais.
+    div(style="width:55%")
+</template>
+
+<style scoped>
+.container {
+  background-color: #fff;
+  height: 317px;
+  flex-direction: column;
+  display: flex;
+}
+.interno {
+  max-height: 317px;
+  flex-direction: column;
+  display: flex;
+  margin: 0 auto;
+  position: relative;
+  width: 85%;
+}
+span {
+  color: #18264c;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,6 +3,7 @@ import topo from "src/components/banners/topo.vue";
 import regua from "src/components/banners/regua.vue";
 import apresentacao from "src/components/banners/apresentacao.vue";
 import beneficios from "src/components/beneficios.vue";
+import flutuante from "src/components/flutuante.vue";
 </script>
 
 <template lang="pug">
@@ -11,6 +12,7 @@ q-page-container
   regua
   apresentacao
   beneficios
+  flutuante
 </template>
 
 <style scoped>
