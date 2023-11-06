8526a5ce85a1e6d7a39c6410ac0805bfa40669cb
caiocosta22 <caiocosta22@gmail.com>
2023-11-06 16:27:55 -0300

    ajuste responsividade

--- src/components/flutuante.vue
+++ src/components/flutuante.vue
@@ -4,15 +4,9 @@
 <template lang="pug">
 div.container
   div.interno
-    div(
-      style="width:45%;"
-    )
-      span(
-        style="font-size:28px; font-weight:400;text-align:left;"
-      ) Tudo o que você precisa em suprimentos corporativos está aqui.&nbsp
-      span(
-        style="font-size:28px; font-weight:bold;text-align:left;"
-      ) Papelaria, limpeza, descartáveis, EPI, equipamentos e muito mais.
+    div
+      span.texto1 Tudo o que você precisa em suprimentos corporativos está aqui.&nbsp
+      span.texto2 Papelaria, limpeza, descartáveis, EPI, equipamentos e muito mais.
 </template>
 
 <style scoped>
@@ -32,7 +26,49 @@ div.container
   align-items: center;
   justify-content: space-between;
 }
+.interno > div {
+  width: 45%;
+}
 span {
   color: #18264c;
 }
+.texto1 {
+  font-size:28px;
+  font-weight:400;
+  text-align:left;
+}
+.texto2 {
+  font-size:28px;
+  font-weight:bold;
+  text-align:left;
+}
+@media screen and (max-width: 1024px) {
+  .texto1 {
+  font-size:24px;
+}
+  .texto2 {
+  font-size:24px;
+}
+}
+@media screen and (max-width: 769px) {
+  .texto1 {
+  font-size:24px;
+}
+ .texto2 {
+  font-size:24px;
+}
+.interno > div {
+  width: 100%;
+  text-align: center;
+}
+}
+@media screen and (max-width: 480px) {
+  .texto1 {
+  font-size:20px;
+}
+ .texto2 {
+  font-size:20px;
+}
+}
+
 </style>
