801904dca26ae04ed7186559746debea5a0e0ac1
caiocosta22 <caiocosta22@gmail.com>
2023-11-06 17:10:47 -0300

    ajuste responsivo

--- src/components/suprimentos.vue
+++ src/components/suprimentos.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 const icons = ref([
   { icone: "/images/suprimentos/1.png", tag: "Escolar" },
   { icone: "/images/suprimentos/2.png", tag: "Escritório" },
@@ -11,26 +13,74 @@ const icons = ref([
   { icone: "/images/suprimentos/8.png", tag: "Informática" },
   { icone: "/images/suprimentos/9.png", tag: "Eletroportáteis" }
 ]);
+const settings = ref({
+  snapAlign: "start",
+  wrapAround: true
+});
+const breakpoints = ref({
+  368: {
+    itemsToShow: 2.0
+  },
+  558: {
+    itemsToShow: 2.0
+  },
+  768: {
+    itemsToShow: 3.0
+  },
+  1024: {
+    itemsToShow: 4.0
+  },
+  1280: {
+    itemsToShow: 6.0
+  }
+});
 </script>
 
 <template lang="pug">
 div.container
   div.interno.q-pt-xl
-    p.q-pb-md(style="font-weight:700; font-size: 34px; text-align:left") TUDO EM SUPRIMENTOS PARA O SEU NEGÓCIO
-    div.row.flex.justify-between(style="width:100%; position: relative;")
-      div(
-        v-for="(icon,index) in icons"
-        :key="index"
+    p.q-pb-md.titulo TUDO EM SUPRIMENTOS PARA O SEU NEGÓCIO
+  Carousel(
+    v-bind="settings"
+    :breakpoints="breakpoints"
+    style="width: 85%; margin: 0 auto;"
+  )
+    Slide(
+    v-for="(icon,index) in icons"
+    :key="index"
+    )
+      div.column(
+        style="height: 100%;"
       )
-        img(
+        div(
+          style="height: 80%;"
+        )
+          img.carrossel(
           :src="icon.icone"
           spinner="white"
-          style="max-width:100%;display:block;height:150px"
+          style="width: 160px; margin:0 auto;"
+          )
+        div.q-pb-lg(
+          style="height: 20%;"
         )
-        p.q-pt-sm(style="text-align:center; max-width:200px; font-size:18px") {{ icon.tag }}.
+          p(
+          style="margin: 0 auto;"
+          ) {{ icon.tag }}
 </template>
 
 <style scoped>
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+}
+.carousel__slide p {
+  height: 10%
+}
+.titulo {
+  font-weight:700;
+  font-size: 34px;
+  text-align:center
+}
 .container {
   background-color: #ffed00;
   height: 560px;
@@ -53,5 +103,14 @@ p {
 }
 span {
   color: #18264c;
+}@media (max-width: 1024px) {
+  .titulo {
+    font-size: 30px;
+  }
+}
+@media (max-width: 480px) {
+  .titulo {
+    font-size: 24px;
+  }
 }
 </style>
