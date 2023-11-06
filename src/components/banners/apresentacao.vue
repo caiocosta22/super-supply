a7b2ac642accae0cb56fd1a2bf58664952f24519
caiocosta22 <caiocosta22@gmail.com>
2023-11-06 10:13:08 -0300

    inicio responsividade sessaoquemsomos

--- src/components/banners/apresentacao.vue
+++ src/components/banners/apresentacao.vue
@@ -6,16 +6,20 @@ div.container
   div.justify-center.q-pt-lg
     p(style="font-weight:700; font-size: 34px; text-align:center") QUEM SOMOS
   div.interno.q-pt-xl
-    div(style="width:40%")
-      p(style="font-size:32px; font-weight:700;") Descubra uma abordagem moderna e inovadora, criada  especialmente para empresas como a sua.
-    div(style="width:20%")
-      q-separator(
+    div.coluna
+      p.texto1 Descubra uma abordagem moderna e inovadora, criada  especialmente para empresas como a sua.
+    div.barras
+      q-separator#barra1(
         color="yellow"
         vertical
         style="width:12px; height:310px; margin: 0 auto"
       )
-    div(style="width:40%")
-      p(style="font-size:19px; font-weight:600; text-align:left") A Super Supply é uma distribuidora de suprimentos empresariais com foco em contratos corporativos. Independentemente do tipo do seu negócio, nosso mix de produtos foi pensado para atender às suas necessidades. Simplificamos suas compras de suprimentos corporativos para que você possa se concentrar no que faz de melhor. Estamos prontos para servi-lo de maneira eficaz e confiável.
+      q-separator#barra2(
+        color="yellow"
+        style="height:10px; margin: 0 auto"
+      )
+    div.coluna
+      p.texto2 A Super Supply é uma distribuidora de suprimentos empresariais com foco em contratos corporativos. Independentemente do tipo do seu negócio, nosso mix de produtos foi pensado para atender às suas necessidades. Simplificamos suas compras de suprimentos corporativos para que você possa se concentrar no que faz de melhor. Estamos prontos para servi-lo de maneira eficaz e confiável.
 </template>
 
 <style scoped>
@@ -37,8 +41,53 @@ div.container
   width: 85%;
   justify-content: space-between;
 }
+.texto1{
+  font-size:32px;
+  font-weight:700;
+}
+.coluna {
+  width:40%
+}
+.barras {
+  width:20%
+}
+.texto2{
+  font-size:19px;
+  font-weight:600;
+  text-align:left
+}
 p {
   margin: 0 auto;
   color: #18264c;
+}
+@media screen and (max-width: 1024px) {
+  .texto1 {
+    font-size: 28px;
+  }
+  .texto2 {
+    font-size: 16px;
+  }
+}
+@media screen and (max-width: 768px) {
+  #barra1 {
+    display: none;
+  }
+  .interno {
+    flex-direction: column;
+  }
+  .coluna {
+    width: 85%;
+  }
+  .barras {
+    width: 85%;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+}
+@media screen and (min-width: 768px) {
+  #barra2 {
+    display: none;
+  }
+
 }
 </style>
