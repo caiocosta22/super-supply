c9b301406b834043078dd8df731e3b8f50900f87
Caio Costa <caiocosta22@gmail.com>
2024-03-05 10:21:25 -0300

    ajuste

--- src/components/localizacao.vue
+++ src/components/localizacao.vue
@@ -4,7 +4,7 @@
 <template lang="pug">
 div.container
   div.interno
-    .titulo NOSSA LOJA
+    .titulo ONDE ESTAMOS
     iframe(
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.8343726477356!2d-38.50497892502561!3d-3.845707096128084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c7511e24716cc1%3A0x5f87038ea67a5450!2sSuper%20Supply!5e0!3m2!1spt-BR!2sbr!4v1709593384901!5m2!1spt-BR!2sbr"
       style="border:0; width:100%; height:100%"
